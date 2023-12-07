dd53508ff2bf1f40093840a25cc639080de44f1d
5CYTH3 <ScytheD@outlook.fr>
2023-12-08 00:04:05 +0100

    Nothing much to say, just added a lil comment.

--- config.sh
+++ config.sh
@@ -4,3 +4,5 @@ ln -s ~/.dotfiles/waybar ~/.config/waybar
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/fish ~/.config/fish
 ln -s ~/.dotfiles/hypr ~/.config/hypr
+
+# TODO: omf, kitty, rofi
